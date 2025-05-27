d3270b34f15bfc33243f24f385e9372fac77dd4f
kaboreibrahim <ibrahimkabore025@gmail.com>
2025-05-27 14:27:14 +0000

    first commit

--- .history/porto_folio/settings_20250424095028.py
+++ .history/porto_folio/settings_20250424095028.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mon-portfolio-dh55.onrender.com','localhost','127.0.0.1','portofolio-8a15.onrender.com']
+ALLOWED_HOSTS = ['mon-portfolio-dh55.onrender.com','localhost','127.0.0.1']
 
 
 # Application definition
